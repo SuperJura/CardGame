72b268cb4d788c355309c779ed74ae74e32520f6
SuperJura <jura1994jk@hotmail.com>
2016-05-26 11:52:32 +0200

    Starten new mode

---
+++ Game/Assets/Scripts/Adventure/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Starting combat");
+    }
+}
