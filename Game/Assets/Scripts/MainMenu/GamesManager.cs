ee9568d4e0eb3b4e1df69472617f4f5279018cd5
SuperJura <jura1994jk@hotmail.com>
2016-08-22 20:51:58 +0200

    Refactored alot of menu code 2

--- Game/Assets/Scripts/MainMenu/GamesManager.cs
+++ Game/Assets/Scripts/MainMenu/GamesManager.cs
@@ -1,31 +1,46 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GamesManager : MonoBehaviour
+public static class GamesManager
 {
-    public void LoadCoopGame()
+    public static void LoadCoopGame()
     {
         Deck.CheckCards();
         SceneManager.LoadScene("CoopGame");
     }
 
-    public void LoadBotGame()
+    public static void LoadBotGame()
     {
         SceneManager.LoadScene("BotGame");
     }
 
-    public void LoadOnlineGame()
+    public static void LoadOnlineGame()
     {
         Deck.CheckCards();
         SceneManager.LoadScene("OnlineGame");
     }
 
-    public void LoadMainMenu()
+    public static void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void ExitApplication()
+    public static string GetApplicationPath()
+    {
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)    return Application.persistentDataPath;
+        return Application.dataPath;
+    }
+
+    public static Enumerations.GameModes GetGameMode()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName.StartsWith("Bot")) return Enumerations.GameModes.Bot;
+        if (sceneName.StartsWith("Coop")) return Enumerations.GameModes.Coop;
+        if (sceneName.StartsWith("Online")) return Enumerations.GameModes.Online;
+        return Enumerations.GameModes.MainMenu;
+    }
+
+    public static void ExitApplication()
     {
         Application.Quit();
     }
